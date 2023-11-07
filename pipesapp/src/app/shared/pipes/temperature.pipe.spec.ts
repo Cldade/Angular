f01cb6e071347ba7ea16fd35e5e9f9a39e84ae63
Claudia Daras de la Fuente <cladade@mercadona.es>
2023-11-07 12:46:14 +0100

    pruebas pipes

--- pipesapp/src/app/shared/pipes/temperature.pipe.spec.ts
+++ pipesapp/src/app/shared/pipes/temperature.pipe.spec.ts
@@ -1,8 +1,19 @@
+import { TemperatureValues } from '../enums/temperature.enum';
 import { TemperaturePipe } from './temperature.pipe';
 
 describe('TemperaturePipe', () => {
   it('create an instance', () => {
     const pipe = new TemperaturePipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('debería retornar grados C si mando grados C', () => {
+    const pipe = new TemperaturePipe();
+    expect(pipe.transform(12, TemperatureValues.C)).toEqual('12 °C');
+  });
+
+  it('debería retornar grados C si mando grados C', () => {
+    const pipe = new TemperaturePipe();
+    expect(pipe.transform(12, TemperatureValues.F)).toEqual('53.6 °F');
   });
 });
