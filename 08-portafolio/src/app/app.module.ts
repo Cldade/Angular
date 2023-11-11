c6ba6930161843938208a05c4981e4018eb9fda0
Cldade <33662701+Cldade@users.noreply.github.com>
2023-11-11 11:21:15 +0100

    Creadno proyecto: portfolio

---
+++ 08-portafolio/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ShareModule } from './shared/components/share.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ShareModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ 08-portafolio/src/app/shared/data/proyectos.data.ts
@@ -0,0 +1,26 @@
+export const proyectos: any[] = [
+    {
+      nombre: 'Contador',
+      img: '/assets/contador.png',
+      tecnologias: ['Angular Framework', 'CSS', 'PWA'],
+      descripcion:
+        'Aplicacion de contador aprendiendo Angular y a manejar eventos y reactividad',
+      url: 'https://agitated-ritchie-67d0dc.netlify.app/',
+    },
+    {
+      nombre: 'Pokeapp',
+      img: '/assets/pokeapp.png',
+      tecnologias: ['Angular Framework', 'Angular Material', 'PWA'],
+      descripcion:
+        'Aplicacion de pokemon usando Angular Material para los estilos',
+      url: 'https://vibrant-jennings-9590cf.netlify.app/',
+    },
+    {
+      nombre: 'Listado de Tareas',
+      img: '/assets/tareas.png',
+      tecnologias: ['Angular Framework', 'Bulma CSS'],
+      descripcion:
+        'Aplicacion de listado de tareas usando Bulma CSS como Framework para los estilos',
+      url: 'https://thirsty-elion-506cc8.netlify.app/',
+    },
+  ];
